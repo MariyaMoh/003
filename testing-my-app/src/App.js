5208341fab7c2ed16d89ebe73781298bf4788625
MariyaMoh <alshaibi199732@gmail.com>
2022-02-28 10:53:12 -0800

    last commit :)

--- testing-my-app/src/App.js
+++ testing-my-app/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul className="web-tooling">
+        <li>Vitor</li>
+        <li>Hussain</li>
+        <li>Mariya</li>
+        <li>Iman</li>
+        <li>Brenda</li>
+      </ul>
     </div>
   );
 }
 
 export default App;
+
+// const Innercomp = ({ value }) => <pre>{value}</pre>;
+// const CounterBut = ({ onClick, children }) => (
+//   <button onClick={onClick}>{children}</button>
+// );
+// const App = () => {
+//   const [number, setNumber] = useState(0);
+//   return (
+//     <div className="App">
+//       <CounterBut onClick={() => setNumber(number - 1)}>-</CounterBut>
+//       <Innercomp value={number} />
+//       <CounterBut onClick={() => setNumber(number + 1)}>+</CounterBut>
+//     </div>
+//   );
+// };
--- testing-my-app/src/App.test.js
+++ testing-my-app/src/App.test.js
@@ -1,8 +1,23 @@
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('list contains 5 names', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const listElement = screen.getByRole('list');
+  const listItems = screen.getAllByRole('listitem');
+
+  expect(listElement).toBeInTheDocument();
+  expect(listElement).toHaveClass('web-tooling');
+  expect(listItems.length).toEqual(5);
 });
